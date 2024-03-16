f1f137489838696d03ef4be08194972f482b0f3d
Ananas2000 <egorkalashnikov8@gmail.com>
2024-03-16 19:34:58 +0300

    matrixInit

---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,23 @@
+#ifndef UNTITLED_MATRIX_H
+#define UNTITLED_MATRIX_H
+
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+matrix getMemMatrix(int nRows, int nCols);
+
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+void freeMemMatrix(matrix *m);
+
+void freeMemMatrices(matrix *ms, int nMatrices);
+
+#endif
