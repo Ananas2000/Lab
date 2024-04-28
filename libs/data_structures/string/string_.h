58a29635bafd58777e4d64bef87fcac2897a5cda
Ananas2000 <egorkalashnikov8@gmail.com>
2024-04-28 13:56:30 +0300

    worDescriptor | digits_to_end

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -1,6 +1,18 @@
 #ifndef UNTITLED_STRING_H
 #define UNTITLED_STRING_H
+
+#define MAX_STRING_SIZE 100
+#define MAX_N_WORDS_IN_STRING 100
+#define MAX_WORD_SIZE 20
+
 #include <stdio.h>
+#include <stdbool.h>
+
+typedef struct WordDescriptor {
+    char *begin;
+    char *end;
+
+} WordDescriptor;
 
 size_t strlen_(const char *begin);
 
@@ -105,6 +117,22 @@ void test_removeExtraSpaces_3();
 void removeAdjacentEqualLetters(char *s);
 
 void test_removeAdjacentEqualLetters();
+
+int getWord(char *beginSearch, WordDescriptor *word);
+
+void digitToStart(WordDescriptor word);
+
+void digitToEnd(WordDescriptor word);
+
+void digitToEndAndReverse(WordDescriptor word);
+
+void digits_to_end(char *string);
+
+void digits_to_end_and_reverse(char *string);
+
+void letters_to_end(char *string);
+
+void test_WordDescriptor();
 
 
 #endif
